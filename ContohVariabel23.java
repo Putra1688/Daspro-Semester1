bc3cf9fb7fcc8acad89531b9ead3a3e762e6b0c7
Putra1688 <ranggadsaputra11@gmail.com>
2023-09-22 13:26:42 +0700

    jobsheetsss

---
+++ ContohVariabel23.java
@@ -0,0 +1,17 @@
+public class ContohVariabel23{
+    public static void main(String[] args) {
+String salahSatuHobySayaAdalah = "Bermain petak umpet";
+boolean isPandai = true;
+char jenisKelamin = 'L';
+byte _umurSayaSekarang = 20;
+double ipk = 3.24; 
+double tinggi = 1.78;
+System.out.println(salahSatuHobySayaAdalah);
+System.out.println("Apakah pandai? " + isPandai);
+System.out.println("Jenis kelamin: " + jenisKelamin);
+System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+System.out.println("IPK saya: " + ipk);
+System.out.println("Tinggi saya: " + tinggi);
+// System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
---
+++ Segitiga23.java
@@ -0,0 +1,14 @@
+public class Segitiga23{
+    public static void main(String[] args) {
+       import java.util.Scanner;
+       Scanner sc = new Scanner(System.in);
+       int alas, tinggi;
+       float luas;
+       System.out.println("Masukkan alas: ");
+       alas = sc.nextInt();
+       System.out.println("Masukkan tinggi: ");
+       tinggi = sc.nextInt();
+       luas = alas * tinggi / 2;
+       System.out.println("Luas Segitiga: " + luas);
+    }
+}
