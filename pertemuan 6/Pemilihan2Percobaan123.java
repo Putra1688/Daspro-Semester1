7fa94a09f36526e2a3bc524dd3f2a4ad5c0eaf8e
Putra1688 <ranggadsaputra11@gmail.com>
2023-10-02 12:42:12 +0700

    modifikasi pemilihan bersarang

--- pertemuan 6/Pemilihan2Percobaan123.java
+++ pertemuan 6/Pemilihan2Percobaan123.java
@@ -7,10 +7,15 @@ public class Pemilihan2Percobaan123 {
         int tahun = input23.nextInt();
 
         if ((tahun % 4) == 0) {
-            if((tahun % 100) !=0)
+            if((tahun % 100) !=0){
             System.out.println("Tahun Kabisat");
-            else 
-            System.out.println("Bukan Tahun Kabisat");
+            }else{
+                if ((tahun % 400) == 0){
+                    System.out.println("Tahun Kabisat");
+                }else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }   
         } else
         System.out.println("Bukan Tahun Kabisat");
     }
