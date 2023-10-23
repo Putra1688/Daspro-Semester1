b6fbf6b89a844ca0ef034ee5294ec54f8d3b3764
Putra1688 <ranggadsaputra11@gmail.com>
2023-10-23 14:40:40 +0700

    Rata Nilai mod

--- pertemuan 9/ArrayRataNilai.java
+++ pertemuan 9/ArrayRataNilai.java
@@ -3,19 +3,34 @@ public class ArrayRataNilai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // array nilaiMhs bertipe integer 
-        int [] nilaiMhs = new int[10];
+        // inisialisasi array dasar bertipe integer dan double
+        int Mhs;
+        int jml_lulus=0, jml_tdklulus=0;
         double total=0;
-        double rata2;
+        double rata2=0;
+        double rata2lulus=0, rata2tdklulus=0;
 
+        System.out.println("Masukkan jumlah Mahasiswa: ");
+        Mhs = sc.nextInt();
+
+        // menentukan kapasitas berdasarkan input 
+        int [] nilaiMhs = new int[Mhs];
         for (int i=0; i < nilaiMhs.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" +(i+1) + " : ");
             nilaiMhs [i]= sc.nextInt();
         }
         for (int i=0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata Nilai: " +rata2);
+            if (nilaiMhs[i]>70) {
+            rata2lulus += nilaiMhs[i];
+            jml_lulus++;
+        } else {
+            rata2tdklulus += nilaiMhs[i];
+            jml_tdklulus++;
+         }
+    }
+        
+        System.out.println("Rata-rata Nilai Lulus      : " +rata2lulus/jml_lulus);
+        System.out.println("Rata-rata Nilai Tidak Lulus: " +rata2tdklulus/jml_tdklulus);
+        
     }
 }
