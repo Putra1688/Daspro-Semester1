480bfe1c48f4eefffe9da64b9166e2523ba22a94
Putra1688 <ranggadsaputra11@gmail.com>
2023-10-23 09:24:15 +0700

    array

---
+++ pertemuan 9/ArrayBilangan23.java
@@ -0,0 +1,17 @@
+public class ArrayBilangan23 {
+    public static void main(String[] args) {
+        
+    // array bertipe integer dengan kapasitas 4 elemen
+    int [] bil = new int [4];
+    bil [0] = 5;
+    bil [1] = 13;
+    bil [2] = -7;
+    bil [3] = 17;
+
+    //menampilkan ke layar
+    System.out.println(bil[0]);
+    System.out.println(bil[1]);
+    System.out.println(bil[2]);
+    System.out.println(bil[3]);
+    }
+}
