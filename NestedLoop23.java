3d1a5becf1b48ff925a8b7683cffc48492d41380
Putra1688 <ranggadsaputra11@gmail.com>
2023-11-06 15:58:57 +0700

    NestedLoop

--- NestedLoop23.java
+++ NestedLoop23.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class NestedLoop_Absen {
+public class NestedLoop23 {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
@@ -16,10 +16,13 @@ public class NestedLoop_Absen {
 
         for (int i=0;i < temps.length; i++) {
             System.out.println("Kota ke-" +i);
-            for (int j=0; j<temps[0].length; j++) {
+            double jumlah_temp = 0;
+            for (int j=0; j <temps[0].length;j++) {
                 System.out.print(temps[i][j] + " ") ;
+                jumlah_temp += temps[i][j];
             }
-            System.out.println();
+            double rata2_temp = jumlah_temp / temps[i].length;
+            System.out.println("Rata-rata adalah : " +rata2_temp);
         }
     }
 }
